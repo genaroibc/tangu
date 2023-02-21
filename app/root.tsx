e72caab6d032f955752f0f085eb9e87d6c1b43e8
genaroibc <bonavitaismael@gmail.com>
2023-02-21 16:51:12 -0300

    [STYLE]: group imports

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
