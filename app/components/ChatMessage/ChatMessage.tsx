bdede4ed23f488ec59efb4dfa8498565e21afdfd
genaroibc <bonavitaismael@gmail.com>
2023-02-21 14:58:58 -0300

    [DEV]: add ChatMessage component

---
+++ app/components/ChatMessage/ChatMessage.tsx
@@ -0,0 +1,19 @@
+import type { Database } from "~/types/db";
+
+export function ChatMessage({
+  content,
+  created_at,
+  id
+}: Database["public"]["Tables"]["messages"]["Row"]) {
+  const isOwn = Math.random() > 0.5;
+  const ownerShipClassName = isOwn
+    ? "chat_message_own"
+    : "chat_message_forgein";
+
+  return (
+    <div className={`chat_message ${ownerShipClassName}`}>
+      <p className="chat_message__content">{content}</p>
+      <time className="chat_message__date">{created_at}</time>
+    </div>
+  );
+}
