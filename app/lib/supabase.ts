5e422fee05140e6a411bba965dac330ebe6409f6
genaroibc <bonavitaismael@gmail.com>
2023-02-21 12:49:18 -0300

    [DEV]: return supabase execution and move validations outside of it

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,13 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "~/types/db";
 
-export function supabase() {
-  const SUPABASE_URL = process.env.SUPABASE_URL;
-  const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 
-  if (!SUPABASE_URL)
-    throw new Error("'SUPABASE_URL' env variable is not defined");
-  if (!SUPABASE_ANON_KEY)
-    throw new Error("'SUPABASE_ANON_KEY' env variable is not defined");
+if (!SUPABASE_URL)
+  throw new Error("'SUPABASE_URL' env variable is not defined");
+if (!SUPABASE_ANON_KEY)
+  throw new Error("'SUPABASE_ANON_KEY' env variable is not defined");
 
-  return createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-}
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
