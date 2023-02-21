1daf0e63baadc9fc678032a3817ebcc0ab021dcf
genaroibc <bonavitaismael@gmail.com>
2023-02-21 12:52:49 -0300

    [DEV]: move styles from home page to root

--- app/root.tsx
+++ app/root.tsx
@@ -1,17 +1,23 @@
 import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  ScrollRestoration
 } from "@remix-run/react";
+import globalStyles from "~/styles/global.css";
+
+export const links: LinksFunction = () => [
+  { href: globalStyles, rel: "stylesheet" }
+];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Tangu Chat",
-  viewport: "width=device-width,initial-scale=1",
+  viewport: "width=device-width,initial-scale=1"
 });
 
 export default function App() {
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,3 @@
-import type { LinksFunction } from "@remix-run/node";
-import globalStyles from "~/styles/global.css";
-
-export const links: LinksFunction = () => [
-  { href: globalStyles, rel: "stylesheet" }
-];
-
 export default function Index() {
   return (
     <main style={{ fontFamily: "Syne, Raleway", lineHeight: "1.4" }}>
