773455a2acf741d3179e48c9ff4dea170aa61ee2
genaroibc <bonavitaismael@gmail.com>
2023-03-08 20:03:16 -0300

    [DEV]: add hook to use supabase

---
+++ app/hooks/useSupabaseClient.ts
@@ -0,0 +1,12 @@
+import { useOutletContext } from "@remix-run/react";
+import { type OutletContext } from "~/types";
+
+export function useSupabaseClient() {
+  const { supabaseClient } = useOutletContext<OutletContext>();
+
+  if (!supabaseClient) {
+    throw new Error("'supabase' property from useOutletContext is not defined");
+  }
+
+  return supabaseClient;
+}
