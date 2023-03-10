2419a571116ce89d111813681a1316e7d3a2a22c
genaroibc <bonavitaismael@gmail.com>
2023-03-10 19:20:01 -0300

    [FEAT]: add Login, ChatTab, and Aside components to the root route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,36 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Aside } from "~/components/Aside/Aside";
+import { ChatTab } from "~/components/ChatTab/ChatTab";
+import { Login } from "~/components/Login/Login";
+import { createSupabaseServerClient } from "~/lib/supabase.server";
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const response = new Response();
+
+  const supabase = createSupabaseServerClient(request, response);
+
+  const { data } = await supabase.from("messages").select();
+
+  return json({ messages: data ?? [] }, { headers: response.headers });
+};
+
 export default function Index() {
+  const { messages } = useLoaderData<typeof loader>();
+
   return (
-    <main style={{ fontFamily: "Syne, Raleway", lineHeight: "1.4" }}>
+    <>
       <h1>Tangu Chat</h1>
-    </main>
+
+      <nav>
+        <Login />
+      </nav>
+
+      <main className="main_container">
+        <ChatTab messages={messages} />
+        <Aside chatList={[]} />
+      </main>
+    </>
   );
 }
