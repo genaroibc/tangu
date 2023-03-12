a25f81d068731f3ffb80676dca728b0f28e7d527
genaroibc <bonavitaismael@gmail.com>
2023-03-12 11:48:45 -0300

    [DEV]: add css custom properties to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",
+        "secondary-color": "var(--secondary-color)",
+        "tertiary-color": "var(--tertiary-color)",
+        "complement-color": "var(--complement-color)",
+
+        "chat-bg-color": "var(--chat-bg-color)",
+        "chat-message-own-bg-color": "var(--chat-message-own-bg-color)",
+        "chat-message-foreign-bg-color": "var(--chat-message-foreign-bg-color)",
+        "chat-list-item-bg-color": "var(--chat-list-item-bg-color)",
+      },
+    },
   },
   plugins: [],
 };
